69b53c4461d534491e5c2c78cc3da38a308c5184
Roox20 <113530291+Roox20@users.noreply.github.com>
2024-08-03 22:14:39 +0600

    Update ayan.js

--- scripts/cmds/ayan.js
+++ scripts/cmds/ayan.js
@@ -2,7 +2,7 @@ module.exports = {
     config: {
         name: "ayan",
         version: "1.0",
-        author: "MR.AYAN", //** original author fb I'd : https://m.me/NOOBS.DEVELOPER.AYAN **//
+        author: "MR.AYAN", //** original author fb I'd : https://m.me/MR.AYAN.2X **//
         countDown: 5,
         role: 0,
         shortDescription: "No Prefix",
@@ -15,6 +15,6 @@ onChat: async function({
     message,
     getLang
 }) {
-    if (event.body && event.body.toLowerCase() == "ayan") return message.reply("𝗔𝘆𝗮𝗻 𝗕𝗼𝘀𝘀 𝗸 𝗮𝗸𝘁𝗮 𝗴𝗶𝗿𝗹𝗳𝗿𝗶𝗲𝗻𝗱 𝗱𝗲𝘂 𝗮𝗺𝗿 𝗔𝘆𝗮𝗻 𝗯𝗼𝘀𝘀 𝗽𝘂𝗿𝗲 𝘀𝗶𝗻𝗴𝗹𝗲-!!🥺😷");
+    if (event.body && event.body.toLowerCase() == "ayan") return message.reply("আমাকে বলতে পারেন বস ঘুমাইছে-!!🥀");
 }
-};
+}; 
